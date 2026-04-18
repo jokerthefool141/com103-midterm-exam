820853d8dbc75d7463fc6084338b4618abaaf713
Jaypee James Isip <jaypeejames.isip@fcpc.edu.ph>
2026-04-18 11:41:59 +0800

    Changes to the program to display categories using a loop

--- midterm_solution.py
+++ midterm_solution.py
@@ -3,13 +3,30 @@ print("\tSTUDENT WEEKLY BUDGET TRACKER")
 print("=" * 40)
 print()
  
-student_name = input("Enter your name: ").title
-weekly_budget = float(input("Enter your weekly budget: "))
+student = input("Student name: ").title()
+budget = float(input("Weekly budget (number): "))
+
+print()
 
 categories = [
-    ["Food and Drinks", "Lunch, snacks, coffee"]
-    ["Transportation", "Bus, jeepney, ride-share"]
-    ["Mobile / Internet", "Load, data plan, WiFi top-up"]
-    ["School Supplies", "Notebook, pen, bond paper"]
+    ["Food & Drinks", "Lunch, snacks, coffee"],
+    ["Transportation", "Bus, jeepney, ride-share"],
+    ["Mobile / Internet", "Load, data plan, WiFi top-up"],
+    ["School Supplies", "Notebook, pen, bond paper"],
     ["Entertainment", "Games, movies, hangout"]
 ]
+
+print("=" * 50)
+print("\t  WEEKLY EXPENSE -- CATEGORIES")
+print("=" * 50)
+print(f"{'No':<3} {'Category':<20} {'Example Expenses'}")
+print()
+
+for i, cat in enumerate(categories, 1):
+    print(f"{i:<3} {cat[0]:<20} {cat[1]}")
+print("=" * 50)
+print()
+
+expenses = []
+total_spent = 0
+

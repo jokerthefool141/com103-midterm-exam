405fc13f1654c45c532944527ae086242b14a479
Jaypee James Isip <jaypeejames.isip@fcpc.edu.ph>
2026-04-18 11:16:03 +0800

    Added inputs and lists in the midterm exam program

---
+++ midterm_solution.py
@@ -0,0 +1,15 @@
+print("=" * 40)
+print("\tSTUDENT WEEKLY BUDGET TRACKER")
+print("=" * 40)
+print()
+ 
+student_name = input("Enter your name: ").title
+weekly_budget = float(input("Enter your weekly budget: "))
+
+categories = [
+    ["Food and Drinks", "Lunch, snacks, coffee"]
+    ["Transportation", "Bus, jeepney, ride-share"]
+    ["Mobile / Internet", "Load, data plan, WiFi top-up"]
+    ["School Supplies", "Notebook, pen, bond paper"]
+    ["Entertainment", "Games, movies, hangout"]
+]
